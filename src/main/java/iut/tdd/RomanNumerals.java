42adc1b452395e92d720b01b36df43880dfb33f8
Baptiste <baptiste.plaisance@hotmail.com>
2016-04-21 15:09:27 +0200

    first commit

---
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -0,0 +1,19 @@
+package iut.tdd;
+
+public class RomanNumerals {
+	public String convertToRoman(String arabe) {
+		return "I";
+	}
+
+	public Object convertFromRoman(String roman) {
+		return "1";
+	}
+	public String convertToRoman(String arabe) {
+		return "II";
+		
+	}
+	public Object convertFromRoman(String roman) {
+		return "2";
+	}
+
+}
---
+++ src/test/java/iut/tdd/RomanNumeralsTest.java
@@ -0,0 +1,26 @@
+package iut.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RomanNumeralsTest {
+
+	@Test
+	public void should_return_I_when_1 () {
+		//Given
+		String input = "1";
+		String expected = "I";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		//When
+		String actual = romanNumeral.convertToRoman(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
+	public void should_return_I_when_2 () {
+		String input = "2";
+		String expected="II";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		Assert.assertEquals(expected, actual);
+	}
+	
+}
